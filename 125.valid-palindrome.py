50dcdb13ac0f3dc5171ad0a340884681015a96de
Trevor White <trevor.white@wayne.edu>
2023-09-08 14:22:17 -0400

    Added comment for a better solution

--- 125.valid-palindrome.py
+++ 125.valid-palindrome.py
@@ -9,11 +9,12 @@ class Solution:
     def isPalindrome(self, s: str) -> bool:
         alphanumeric = ""
         for char in s:
+            # char.isalnum() is better, check alpha or numeric in 1 line
             if char.isdigit() or char.isalpha():
                 alphanumeric+=char
         # string immutable so set variable name to output of lower function
         alphanumeric = alphanumeric.lower()
-        # [::-1] reverses a string,
+        # [::-1] reverses a string, copies the string starting at the end, iterating backwards -1 index each time
         if alphanumeric == alphanumeric[::-1]:
             return True
         else:
