8eb2a00808979ce02f0680b339b9670baf4326d6
Trevor White <trevor.white@wayne.edu>
2023-09-08 14:18:15 -0400

    Added comment for a better solution

---
+++ 167.two-sum-ii-input-array-is-sorted.py
@@ -0,0 +1,12 @@
+#
+# @lc app=leetcode id=167 lang=python3
+#
+# [167] Two Sum II - Input Array Is Sorted
+#
+
+# @lc code=start
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        
+# @lc code=end
+
